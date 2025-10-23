e54637dc37ba75232cf0c20743e610a1651fffea
hasan sbeity <th.rider.clan@gmail.com>
2025-10-23 23:33:26 +0300

    /auth redirect

--- next.config.js
+++ next.config.js
@@ -2,6 +2,16 @@
 const nextConfig = {
   reactStrictMode: true,
   output: "standalone",
+
+  async redirects() {
+    return [
+      {
+        source: "/auth",
+        destination: "https://dashboard.vertex-engineering.co",
+        permanent: false, // change to true if you want a 308 permanent redirect
+      },
+    ];
+  },
 };
 
 module.exports = nextConfig;
